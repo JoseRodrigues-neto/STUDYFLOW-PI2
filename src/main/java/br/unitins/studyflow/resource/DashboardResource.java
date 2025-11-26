dcd3db05e0aad9d94eb25ec15ace0a513826809f
Jose neto <joserodrigues15ne@gmail.com>
2025-11-26 12:13:33 -0300

    implementação de DashboardDTO e DashboardService; adicionar métodos de filtragem em DashboardRepository

--- src/main/java/br/unitins/studyflow/resource/DashboardResource.java
+++ src/main/java/br/unitins/studyflow/resource/DashboardResource.java
@@ -1,8 +1,9 @@
 package br.unitins.studyflow.resource;
 
  
-import org.eclipse.microprofile.jwt.JsonWebToken; 
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import br.unitins.studyflow.dto.DashboardDTO;
 import br.unitins.studyflow.service.DashboardService;
 import br.unitins.studyflow.service.UsuarioService;
 import jakarta.annotation.security.RolesAllowed;
@@ -10,6 +11,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -26,13 +28,22 @@ public class DashboardResource {
     @Inject
     UsuarioService usuarioService;
 
-    @GET
-   @Path("/me")
-   @RolesAllowed({"ALUNO", "PROFESSOR"})
-    public Response getDashboardData() {
-        String uid = jwt.getSubject();
+@GET
+@Path("/me")
+@RolesAllowed({"ALUNO", "PROFESSOR"})
+public Response getDashboardData(
+    @QueryParam("roadmapId") Long roadmapId,
+    @QueryParam("from") String from,
+    @QueryParam("to") String to,
+    @QueryParam("order") String order
+) {
+    String uid = jwt.getSubject();
+    Long usuarioId = usuarioService.buscarPorId(uid).id();
 
-        Long usuarioId = usuarioService.buscarPorId(uid).id(); 
-        return Response.ok(dashboardService.getDashboardData(usuarioId)).build();
-    }
+    DashboardDTO dto = dashboardService.getDashboardData(usuarioId);
+
+    return Response.ok(dto).build();
+}
+
+
 }
